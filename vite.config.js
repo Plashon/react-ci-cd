87075b009144fc27bdea929cf9423bfb85d8b775
Plashon <fourza12poobate@gmail.com>
2025-03-14 23:39:54 +0700

    add Home page, layout components, and routing; integrate Tailwind CSS

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base:"/react-ci-cd/",
-  server:{
-    open:true,
-    port:5000,
-  }
-})
+  plugins: [react(), tailwindcss()],
+  base: "/react-ci-cd/",
+  server: {
+    open: true,
+    port: 5000,
+  },
+});
